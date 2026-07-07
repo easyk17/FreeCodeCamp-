32e209cf5adb1c67d4a510b106bff343bb6e9cf8
YOGESH KUMAR <easyk17@gmail.com>
2026-07-08 01:47:30 +0530

    New workshop

---
+++ Buil an Email Simulator.py
@@ -0,0 +1,4 @@
+# Step 1
+# In this workshop, you are going to build an Email Simulator that simulates sending, receiving, and managing emails between different users. You'll learn about classes, objects, and how to organize code in an object-oriented way.
+
+# Begin by creating a class named Email using the class keyword.
